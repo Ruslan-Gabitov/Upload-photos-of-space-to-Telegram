dcef1c8be6031f9cca19054d4a1ff0667dc32086
Ruslan-Gabitov <rgabitov355@gmail.com>
2021-10-28 23:02:05 +0500

    publish photo to Telegram channel

--- app/bot.py
+++ app/bot.py
@@ -9,3 +9,4 @@ if __name__ == '__main__':
     load_dotenv()
     bot = telegram.Bot(token=os.getenv('TG_TOKEN'))
     bot.send_message(chat_id='@cosmo_mo', text='И снова привет!')
+    bot.send_document(chat_id='@cosmo_mo', document=open('download.jpeg', 'rb'), caption='И снова привет!')
