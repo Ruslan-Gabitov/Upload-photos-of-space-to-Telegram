f16df92c6bc5e8ab55e21dfc90285356864c29c0
Ruslan-Gabitov <rgabitov355@gmail.com>
2021-11-02 00:18:28 +0500

    renamed files for better navigation

--- app/main.py
+++ app/main.py
@@ -1,71 +1,21 @@
-import requests
-from pathlib import Path
+import telegram
 from dotenv import load_dotenv
 import os
-from urllib.parse import urlsplit
-import datetime
+import time
 
-
-def get_filename_extension(urls):
-    url = urlsplit(urls)
-    path, filename_extension = os.path.splitext(url.path)
-    return filename_extension
-
-
-def create_folder(name_folder):
-    Path(name_folder).mkdir(parents=True, exist_ok=True)
-    return name_folder
-
-
-def upload_images(urls, path):
-    for id, url in enumerate(urls):
-        response = requests.get(url)
-        response.raise_for_status()
-        filename_extension = get_filename_extension(url)
-        with open(f'{path}{id+1}{filename_extension}', 'wb') as file:
-            file.write(response.content)
-
-
-def fetch_spacex_last_launch(flight_number):
-    url = 'https://api.spacexdata.com/v3/launches/'
-    params = {'flight_number': flight_number}
-    response = requests.get(url, params=params)
-    response.raise_for_status()
-    return response.json()[0]['links']['flickr_images']
-
-
-def fetch_nasa_last_launch(image_namber):
-    url = 'https://api.nasa.gov/planetary/apod'
-    params = {'api_key': os.getenv('NASA_TOKEN'), 'count': image_namber}
-    response = requests.get(url, params=params)
-    response.raise_for_status()
-    urls = response.json()
-    links = [link['url'] for link in urls]
-    return links
-
-
-def fetch_nasa_epic_last_launch():
-    url = 'https://api.nasa.gov/EPIC/api/natural/images'
-    params = {'api_key': os.getenv('NASA_TOKEN')}
-    response = requests.get(url, params=params)
-    response.raise_for_status()
-    links = []
-    date, times = response.json()[0]['date'].split(' ')
-    aDate = datetime.date.fromisoformat(date).strftime("%Y/%m/%d")
-    for namedate in response.json():
-        image = namedate['image']
-        links.append(f'https://api.nasa.gov/EPIC/archive/natural/{aDate}/png/{image}.png?api_key={os.getenv("NASA_TOKEN")}')
-    return links
+def publish_images_to_channel(chat_id, time_sleep=86400):
+    bot = telegram.Bot(token=os.getenv('TG_TOKEN'))
+    count = 0
+    print(len(os.listdir('images')))
+    for img in os.listdir('images'):
+        bot.send_document(chat_id=chat_id, document=open(f'images/{img}', 'rb'), caption='')
+        time.sleep(time_sleep)
+        print(f'Фото {img} опубликованно!')
+        count += 1
+    print(count)
 
 
 if __name__ == '__main__':
     load_dotenv()
-    create_folder('images_nasaepic')
-    create_folder('images_spacex')
-    create_folder('images_nasa')
-    upload_images(urls=fetch_spacex_last_launch(flight_number=90),
-               path='images_spacex/spacex_')
-    upload_images(urls=fetch_nasa_last_launch(
-        image_namber=30), path='images_nasa/nasa_')
-    upload_images(urls=fetch_nasa_epic_last_launch(), path='images_nasaepic/epic_')
+    publish_images_to_channel(time_sleep=3, chat_id='@cosmo_mo')
     
